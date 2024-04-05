915e81b9b5e9ac53d307c579fc590061cd4c3a1b
AmdadulHQ <rimonamdadul301@gmail.com>
2024-04-05 18:56:03 +0600

    adding filter news

--- src/Component/Categorys/Categorys.jsx
+++ src/Component/Categorys/Categorys.jsx
@@ -1,21 +1,29 @@
+import { useEffect, useState } from 'react';
 import pic1 from '../../assets/1.png'
 import pic2 from '../../assets/2.png'
 import pic3 from '../../assets/3.png'
-import { CiCalendar } from "react-icons/ci";
+import { CiCalendar, CiLight } from "react-icons/ci";
 
 const Categorys = ({handleShowNews}) => {
 
+    const[ categorys, setCategorys ] = useState([])
 
 
+    useEffect(()=> {
+        fetch('/categories.json')
+        .then( res => res.json())
+        .then( data => setCategorys(data) )
+    },[])
 
+    console.log(categorys);
     return (
         <div>
             <h1 className="font-semibold text-xl text-Dark1 mb-5">All Caterogy</h1>
             <div>
                 {
-
-                }
-                <button onClick={()=> handleShowNews()} className="block text-xl font-semibold text-Dark1 py-4 bg-Dark3 w-full">National News</button>
+                    categorys.map( category => <button key={category.id} onClick={()=> handleShowNews(category.id)} className="block text-xl font-semibold text-Dark1 py-4 bg-Dark3 w-full">{category.name}</button> )
+                } 
+                {/* <button onClick={()=> handleShowNews()} className="block text-xl font-semibold text-Dark1 py-4 bg-Dark3 w-full">National News</button>
                 <button onClick={()=> handleShowNews(1)} className="block text-xl font-medium text-Dark2 py-4 w-full">Breaking News</button>
                 <button onClick={()=> handleShowNews(2)} className="block text-xl font-medium text-Dark2 py-4 w-full">Regular News</button>
                 <button onClick={()=> handleShowNews(3)} className="block text-xl font-medium text-Dark2 py-4 w-full">International News</button>
@@ -23,7 +31,7 @@ const Categorys = ({handleShowNews}) => {
                 <button onClick={()=> handleShowNews(5)} className="block text-xl font-medium text-Dark2 py-4 w-full">Entertainment</button>
                 <button onClick={()=> handleShowNews(6)} className="block text-xl font-medium text-Dark2 py-4 w-full">Culture</button>
                 <button onClick={()=> handleShowNews(7)} className="block text-xl font-medium text-Dark2 py-4 w-full">Arts</button>
-                <button onClick={()=> handleShowNews(0)} className="block text-xl font-medium text-Dark2 py-4 w-full">All News</button>
+                <button onClick={()=> handleShowNews(0)} className="block text-xl font-medium text-Dark2 py-4 w-full">All News</button> */}
             </div>
             <div>
                 <div className='mb-4 mt-7'>
